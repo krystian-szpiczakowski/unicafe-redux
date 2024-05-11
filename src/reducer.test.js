d490dab09d81c74dd07cd5d20eefa71129474b80
Krystian Szpiczakowski <szpiczakowski.krystian@gmail.com>
2024-05-11 21:18:14 +0200

    Part 6.1 - Implement first tests

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -9,7 +9,6 @@ describe('unicafe reducer', () => {
   }
 
   test('should return a proper initial state when called with undefined state', () => {
-    const state = {}
     const action = {
       type: 'DO_NOTHING'
     }
@@ -24,7 +23,7 @@ describe('unicafe reducer', () => {
     }
     const state = initialState
 
-    deepFreeze(state)
+    Object.freeze(state)
     const newState = counterReducer(state, action)
     expect(newState).toEqual({
       good: 1,
@@ -32,4 +31,94 @@ describe('unicafe reducer', () => {
       bad: 0
     })
   })
+
+  test('ok is incremented', () => {
+    const action = {
+      type: 'OK'
+    }
+    const state = initialState
+
+    Object.freeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+  })
+
+  test('bad is incremented', () => {
+    const action = {
+      type: 'BAD'
+    }
+    const state = initialState
+
+    Object.freeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
+
+  test('counter sums reviews independently', () => {
+    const badAction = {
+      type: 'BAD'
+    }
+
+    const okAction = {
+      type: 'OK'
+    }
+
+    const goodAction = {
+      type: 'GOOD'
+    }
+
+    let state = initialState
+    Object.freeze(state)
+
+    state = counterReducer(state, badAction)
+    state = counterReducer(state, goodAction)
+    state = counterReducer(state, okAction)
+
+    expect(state).toEqual({
+      good: 1,
+      ok: 1,
+      bad: 1
+    })
+  })
+
+  test('reset sets all reviews to 0', () => {
+    const badAction = {
+      type: 'BAD'
+    }
+
+    const okAction = {
+      type: 'OK'
+    }
+
+    const goodAction = {
+      type: 'GOOD'
+    }
+
+    const resetAction = {
+      type: 'ZERO'
+    }
+
+    let state = initialState
+    Object.freeze(state)
+
+    state = counterReducer(state, badAction)
+    state = counterReducer(state, goodAction)
+    state = counterReducer(state, okAction)
+
+    const stateAfterReset = counterReducer(state, resetAction)
+    expect(stateAfterReset).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 0
+    })
+  })
+
 })
